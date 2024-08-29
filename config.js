88e7e3cd5a25c448f8606703609d5afb7bf453a5
HideOnHeart10 <nakzed132@gmail.com>
2024-08-29 18:08:53 +0700

    init project

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '03-09-2002';
+const giftUrl = 'https://www.facebook.com/sumin39200260';
+const eventName = 'Nguyễn Thúy Hường';
+const titleCard = 'Tặng Cậu';
+const contentCard = 'Chúc cậu có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+
+const giftImage = './2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
