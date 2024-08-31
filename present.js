8072ada3dc25fc5aefad28c41c8eeeb2c1a19a47
HideOnHeart10 <nakzed132@gmail.com>
2024-08-31 21:28:24 +0700

    add img

--- present.js
+++ present.js
@@ -12,7 +12,7 @@ function init() {
   graphElem.setAttribute('data-before', eventName);
   document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
   document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
-  // document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
 
   var _giftLink, 
       _giftImg;
